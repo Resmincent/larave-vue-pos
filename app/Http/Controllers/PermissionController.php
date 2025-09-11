672802b0e74dff6a555fcffeafde58c4536cdc34
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2025-09-11 14:58:36 +0700

    create controller untuk role dan permission

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = $request->string('query');
+        $permissions = Permission::when($query, fn($w) => $w->where('name', 'like', "%$query%"))
+            ->orderBy('name')
+            ->paginate(10)
+            ->withQueryString();
+
+        return Inertia::render('Permissions/Index', [
+            'permissions' => $permissions,
+            'filters' => ['query' => $query],
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Permissions/Create', [
+            'permissions' => Permission::orderBy('name')->get(['id', 'name']),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate(
+            [
+                'name' => 'required|string|max:100|unique:permissions,name',
+                'guard_name' => 'required|string|max:50',
+            ]
+        );
+
+        Permission::create($data);
+        return redirect()->route('permissions.index')->with('success', 'Permission created successfully');
+    }
+
+    public function edit(Permission $permission)
+    {
+        return Inertia::render(
+            'Permissions/Edit',
+            [
+                'permission' => $permission
+            ]
+        );
+    }
+
+    public function update(Request $request, Permission $permission)
+    {
+        $data = $request->validate(
+            [
+                'name' => 'required|string|max:100|unique:permissions,name,' . $permission->id,
+                'guard_name' => 'required|string|max:50',
+            ]
+        );
+        $permission->update($data);
+        return redirect()->route('permissions.index')->with('success', 'Permission updated successfully');
+    }
+
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+        return redirect()->back()->with('success', 'Permission deleted successfully');
+    }
+}
---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = $request->string('query');
+        $roles = Role::when($query, fn($w) => $w->where('name', 'like', "%$query%"))
+            ->orderBy('name')
+            ->paginate(10)
+            ->withQueryString();
+
+        return Inertia::render('Roles/Index', [
+            'roles' => $roles,
+            'filters' => ['query' => $query],
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Roles/Create', [
+            'roles' => Role::orderBy('name')->get(['id', 'name']),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:100|unique:roles,name',
+            'guard_name' => 'required|string|max:50',
+        ]);
+
+        Role::create($data);
+        return redirect()->route('roles.index')->with('success', 'Role created successfully');
+    }
+
+    public function edit(Role $role)
+    {
+        return Inertia::render(
+            'Roles/Edit',
+            [
+                'role' => $role
+            ]
+        );
+    }
+
+    public function update(Request $request, Role $role)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:100|unique:roles,name,' . $role->id,
+            'guard_name' => 'required|string|max:50',
+        ]);
+
+        $role->update($data);
+        return redirect()->route('roles.index')->with('success', 'Role updated successfully');
+    }
+
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        return redirect()->back()->with('success', 'Role deleted successfully');
+    }
+}
