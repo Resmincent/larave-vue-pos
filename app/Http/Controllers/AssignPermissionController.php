34b486f54a422072560d5e979efcbe7740da9745
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2025-09-11 15:32:57 +0700

    create assign permission controller

---
+++ app/Http/Controllers/AssignPermissionController.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class AssignPermissionController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('AssignPermission/Index', [
+            'roles' => Role::all(),
+            'permissions' => Permission::all(),
+        ]);
+    }
+
+    public function editRolePermissions(Role $role)
+    {
+        $permissions = Permission::all();
+        $raw_role_permissions = $role->permissions()->get()->toArray();
+        $role_permissions = [];
+        for ($i = 0; $i < count($raw_role_permissions); $i++) {
+            array_push($role_permissions, $raw_role_permissions[$i]['name']);
+        }
+        return Inertia::render('AssignPermission/EditRolePermissions', [
+            'role' => $role,
+            'permissions' => $permissions,
+            'role_permissions' => $role_permissions,
+        ]);
+    }
+
+    public function updateRolePermissions(Request $request)
+    {
+        $request->validate([
+            'role_id' => 'required|exists:roles,id',
+            'update_role_permissions' => 'required|array',
+        ]);
+
+        try {
+            $role = Role::findById($request->role_id);
+
+            if ($role) {
+                $permission = Permission::whereIn('name', $request->update_role_permissions)->get();
+
+                if ($permission->isNotEmpty()) {
+                    $role->syncPermissions($permission);
+                    return redirect()->route('assign_permissions.index')->with('success', 'Permissions updated successfully.');
+                } else {
+                    return redirect()->back()->with('error', 'No valid permissions found to assign.');
+                }
+            } else {
+                return redirect()->back()->with('error', 'Role not found.');
+            }
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Role not found.');
+        }
+    }
+}
